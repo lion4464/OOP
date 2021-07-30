4abb818af32a59648c1b245e6514ba388b3b7783
talablariga <talablarigabinoan@gmail.com>
2021-07-30 16:11:22 +0500

    (The Fan class) #5

---
+++ Fan_ventilyator/TestFan.java
@@ -0,0 +1,12 @@
+package Fan_ventilyator;
+
+public class TestFan {
+     public static void main(String[] args) {
+         Fan f1=new Fan();
+//         f1.setOn(true);
+         f1.setColor("green");
+         f1.setRadius(6);
+         f1.setSpeed(3);
+         System.out.println("f1 = " + f1);
+    }
+}
